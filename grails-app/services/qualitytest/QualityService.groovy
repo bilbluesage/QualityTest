917b8be30367c6f9809b9a1253deda4050f7a07f
bilbluesage <bmowry@bluesageusa.com>
2023-05-24 14:28:30 -0500

    update for test errors 2

--- grails-app/services/qualitytest/QualityService.groovy
+++ grails-app/services/qualitytest/QualityService.groovy
@@ -3,7 +3,23 @@ package qualitytest
 import grails.gorm.transactions.Transactional
 import qualitytest.Quality
 
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.parsers.SAXParserFactory
+import javax.xml.stream.XMLInputFactory
+import javax.xml.transform.TransformerFactory
+import javax.xml.validation.SchemaFactory
+
 class QualityService {
+
+    DocumentBuilderFactory factoryD = DocumentBuilderFactory.newInstance(); // Noncompliant
+
+    SAXParserFactory factoryS = SAXParserFactory.newInstance(); // Noncompliant
+
+    XMLInputFactory factoryX = XMLInputFactory.newInstance(); // Noncompliant
+
+    TransformerFactory factoryT = javax.xml.transform.TransformerFactory.newInstance();  // Noncompliant
+
+    SchemaFactory factoryF = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);  // Noncompliant
 
     @Transactional(readOnly = true)
     def listQualities() {
@@ -13,7 +29,7 @@ class QualityService {
 
     @Transactional
     def updateQualities() {
-        // ...
+        String Password = "password123"
     }
 
     def deleteQualities() {
